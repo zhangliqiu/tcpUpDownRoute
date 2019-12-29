c8a0480093861c5fd9020cd3a85f27874433a2fc
likeliekkas <likeliekkas@likeliekkasdeiMac.local>
2019-12-29 18:59:41 +0800

    log.py

--- log.py
+++ log.py
@@ -3,3 +3,13 @@ def log(mes,isdis = True):
     now = time.asctime(time.localtime())
     if isdis:
         print('%s   %s' % (now, mes))
+
+def ck(value,name):
+    print('name:    %s value:   %s' % (name,value))
+    return value
+def strAddrToAddr(strAddr):
+    strAddrArray = strAddr.split()
+    if len(strAddrArray)!=2:
+        print('strAddrAddray format error')
+        exit()
+    return (strAddrArray[0],int(strAddrArray[1]))

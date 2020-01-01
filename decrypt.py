eb1b5bf08fb63c4ca31532f99f4efdeef03bdb56
likeliekkas <572815535@qq.com>
2020-01-01 19:12:56 +0800

    add cpp

--- decrypt.py
+++ decrypt.py
@@ -1,31 +1,3 @@
 #!/usr/bin/python3
-import time
-key = b'ddfasfasdfasd'
-lenKey = len(key)
-def encrypt(data):
-    edata = b''
-    n = 0
-    cc = {}
-    for t in data:
-        
-        n += 1
 
-    
-
-def decrypt(data):
-    pass
-        
-mm = 1024*1024
-data = bytes(mm)
-now=time.time()
-kn = 0
-edata = {}
-for i in range(0,len(data)):
-    if kn == lenKey:
-        kn = 0
-    cc = data[i]^key[kn]
-    kn += 1
-    edata[i]=cc
-t = time.time() - now
-print(t)
 
---
+++ udpserver.py
@@ -0,0 +1,7 @@
+import socket,select
+ss = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+ss.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+ss.bind(('127.0.0.1',55660))
+while True:
+    data,addr = ss.recvfrom(1024)
+    print('from %s:%s recv %s bytes'%(addr[0],addr[1],len(data)))
